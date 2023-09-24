c750b9e40cb18cca7ca5f54d05219e5ec623abc1
Jeevan Joshi <117514522+JeevanJoshi4434@users.noreply.github.com>
2023-09-24 19:44:14 +0530

    update for admin

---
+++ README.md
@@ -0,0 +1,7 @@
+# Getting Started with EDU
+
+### `npm install`
+use `npm install` to install all the dependencies and then use `npm start` to start the server.
+
+use `git clone https://github.com/JeevanJoshi4434/edu.git` to clone repository
+then `cd ./edu`.
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'assets/css/App.css';
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from 'theme/theme';
+import { ThemeEditorProvider } from '@hypertheme-editor/chakra-ui';
+import Dashboard from './layouts/admin';
+
+ReactDOM.render(
+	<ChakraProvider theme={theme}>
+		<React.StrictMode>
+			<ThemeEditorProvider>
+				<HashRouter>
+					<Switch>
+						<Route path={`/admin`} component={Dashboard} />
+						<Redirect from='/' to='/admin' />
+					</Switch>
+				</HashRouter>
+			</ThemeEditorProvider>
+		</React.StrictMode>
+	</ChakraProvider>,
+	document.getElementById('root')
+);

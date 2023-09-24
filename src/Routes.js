b76d50789e33def6ed639dad5a17516ee067fa1a
Jeevan Joshi <117514522+JeevanJoshi4434@users.noreply.github.com>
2023-09-24 11:44:03 +0530

    sigup page update

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Landing from './Screens/Landing'
 import Login from './Screens/Auth/Login'
+import Signup from './Screens/Auth/Signup'
 const RouteConfig = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Landing />}></Route>
         <Route path='/login' element={<Login />}></Route>
-        <Route path='/signup' element={<Login />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
       </Routes>
     </Router>
   )

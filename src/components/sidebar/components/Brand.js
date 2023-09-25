32554a6c5fe47409674cb051aa7adb7459f23000
Jeevan Joshi <117514522+JeevanJoshi4434@users.noreply.github.com>
2023-09-25 15:17:18 +0530

    Logo problem solved

--- src/components/sidebar/components/Brand.js
+++ src/components/sidebar/components/Brand.js
@@ -13,7 +13,10 @@ export function SidebarBrand() {
 
   return (
     <Flex align='center' direction='column'>
-      <HorizonLogo h='26px' w='175px' my='32px' color={logoColor} />
+      {/* <HorizonLogo h='26px' w='175px' my='32px' color={logoColor} /> */}
+      <div style={{marginTop:"20px"}}>
+      <h1 style={{fontSize:"30px", fontWeight:"500"}} >SEHPATHI</h1>
+      </div>
       <HSeparator mb='20px' />
     </Flex>
   );
